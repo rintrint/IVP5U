4b3953ddeea6a482ee2b07652ef05f2b23c78f43
rintrint <152774318+rintrint@users.noreply.github.com>
2025-06-24 17:22:54 +0900

    UE 5.6

--- Source/IVP5U/Classes/MMDMeshImportData.h
+++ Source/IVP5U/Classes/MMDMeshImportData.h
@@ -16,9 +16,18 @@ enum EMMDNormalImportMethod
  * Import data and options used when importing any mesh from MMD
  */
 UCLASS(config = EditorUserSettings, configdonotcheckdefaults, abstract)
-class UMMDMeshImportData : public UFbxAssetImportData // UMMDAssetImportData
+class UMMDMeshImportData : public UAssetImportData // UMMDAssetImportData
 {
 	GENERATED_UCLASS_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, config, Category = Transform)
+	FVector ImportTranslation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, config, Category = Transform)
+	FRotator ImportRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, config, Category = Transform)
+	float ImportUniformScale;
 
 	/** Enables importing of mesh LODs from MMD LOD groups, if present in the MMD file */
 	UPROPERTY(EditAnywhere, AdvancedDisplay, config, Category = ImportSettings, meta = (OBJRestrict = "true", ImportType = "Mesh", ToolTip = "If enabled, creates LOD models for Unreal meshes from LODs in the import file; If not enabled, only the base mesh from the LOD group is imported"))
