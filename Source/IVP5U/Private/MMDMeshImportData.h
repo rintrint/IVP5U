f468603f40f6f3fa11ea0ae6f9928043ff476c7c
rintrint <152774318+rintrint@users.noreply.github.com>
2026-05-23 15:01:38 +0800

    refactor: remove dead UPROPERTYs from UMMDMeshImportData

--- Source/IVP5U/Private/MMDMeshImportData.h
+++ Source/IVP5U/Private/MMDMeshImportData.h
@@ -13,18 +13,5 @@ class UMMDMeshImportData : public UAssetImportData // UMMDAssetImportData
 {
 	GENERATED_UCLASS_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, config, Category = Transform)
-	FVector ImportTranslation;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, config, Category = Transform)
-	FRotator ImportRotation;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, config, Category = Transform)
-	float ImportUniformScale;
-
-	/** Enables importing of mesh LODs from MMD LOD groups, if present in the MMD file */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, config, Category = ImportSettings, meta = (ImportType = "Mesh", ToolTip = "If enabled, creates LOD models for Unreal meshes from LODs in the import file; If not enabled, only the base mesh from the LOD group is imported"))
-	uint32 bImportMeshLODs : 1;
-
 	virtual bool CanEditChange(const FProperty* InProperty) const override;
 };
